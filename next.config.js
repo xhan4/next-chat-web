f69022b507d9537325078a56e14b82afee944f72
浮名半生 <8817625+xiaomoguiya@user.noreply.gitee.com>
2025-04-14 21:28:13 +0800

    fix:修改github配置及网络请求

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-
+const rewrites = () => {
+  return [
+    {
+      source: "/api/:path*",
+      destination: "http://localhost:8000/api/:path*",
+    },
+  ];
+};
 const nextConfig = {
+  rewrites,
   experimental: {
     appDir: true,
   },
